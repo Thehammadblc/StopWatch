1382031b696378d199c6973a64d0bcb36ade2966
Thehammadblc <159949738+Thehammadblc@users.noreply.github.com>
2024-05-11 21:09:32 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,60 @@
+var min =0
+var sec = 0
+var mili =0
+
+var displayMin =document.getElementById('Displaymin');
+var displaySec =document.getElementById('Displaysec');
+var displayMili =document.getElementById('Displaymili');
+
+var startBtn = document.getElementById("startBtn");
+var stopBtn = document.getElementById("stopBtn");
+var resetBtn = document.getElementById("resetBtn");
+
+function renderTimer(){
+    displayMin.innerHTML=min ;
+    displaySec.innerHTML=sec ;
+    displayMili.innerHTML=mili ;
+}
+
+function timer(){
+    mili++;
+    if(mili==100){
+        sec++;
+        mili=0;
+        if(sec==60){
+            min++;
+            sec=0;
+        }
+    }
+    renderTimer();
+}
+
+var interval;
+function StartTimer(){
+    interval =setInterval(function (){
+        timer();
+    },100)
+    startBtn.disabled=true
+}
+
+function stopTimer(){
+    clearInterval(interval)
+    startBtn.disabled= false
+}
+
+function resetTimer(){
+    min =0
+    sec=0
+    mili=0
+    stopTimer()
+    renderTimer()
+}
+
+
+
+
+
+    
+
+
+
